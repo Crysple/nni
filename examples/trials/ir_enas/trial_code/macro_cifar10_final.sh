310b605653f6753bb5a606efcf1aa8ef6716969e
Crysple <871886504@qq.com>
2019-04-06 17:16:57 +0800

    update NAS

--- examples/trials/ir_enas/trial_code/macro_cifar10_final.sh
+++ examples/trials/ir_enas/trial_code/macro_cifar10_final.sh
@@ -15,11 +15,11 @@ python3 src/cifar10/nni_child_cifar10.py \
   --eval_every_epochs=1 \
   --child_use_aux_heads \
   --child_num_layers=8 \
-  --child_out_filters=36 \
+  --child_out_filters=96 \
   --child_l2_reg=0.0002 \
   --child_num_branches=6 \
   --child_num_cell_layers=5 \
-  --child_keep_prob=0.90 \
+  --child_keep_prob=0.50 \
   --child_drop_path_keep_prob=0.60 \
   --child_lr_cosine \
   --child_lr_max=0.05 \
